804914d0b7723ad19d0ddfc4898f295b5bea8b2d
estebanDeveloperman <estebandeveloper6@gmail.com>
2023-03-04 18:28:43 -0500

    Desarrollado Trending Products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,10 +14,19 @@ import Services from "../services/Services";
 import ProductsList from "../components/UI/ProductsList";
 
 const Home = () => {
-
-  const [data, setData] = useState(products);
+  const [trendingProducts, setTrendingProducts] = useState([]);
+  const [bestSalesProducts, setBestSalesProducts] = useState([]);
 
   const year = new Date().getFullYear();
+
+  useEffect(() => {
+    const filteredProducts = products.filter(
+      (item) => item.category === "chair"
+    );
+
+    setTrendingProducts(filteredProducts);
+  }, []);
+
   return (
     <Helmet title={"Home"}>
       <section className="hero__section">
@@ -55,7 +64,17 @@ const Home = () => {
             <Col lg="12" className="text-center">
               <h2 className="section__title">Trending Products</h2>
             </Col>
-            <ProductsList />
+            <ProductsList data={trendingProducts} />
+          </Row>
+        </Container>
+      </section>
+
+      <section className="best__sales">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2 className="section__title"> Best Sales</h2>
+            </Col>
           </Row>
         </Container>
       </section>
